58f7f66254149b092be7ba91c16ad9b1419ed706
SAYAN DAS <poke19962008@gmail.com>
2017-03-16 09:01:04 +0530

    bug fixes

--- kmean.py
+++ kmean.py
@@ -1,4 +1,4 @@
-import sampler
+import sampler, pickle
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from sklearn.cluster import KMeans
@@ -11,9 +11,9 @@ print "Frame Dimension: ", frames.shape
 
 def pixelDensity(x, y):
     vec = frames[:,x, y]
-    binwidth = 1
+    binwidth = 25
 
-    plt.hist(vec, bins=range(min(vec), max(vec) + binwidth, binwidth), normed=True)
+    plt.hist(vec, binwidth, normed=True)
     plt.xlim((min(vec), max(vec)))
 
     # mean = np.mean(vec)
@@ -22,43 +22,63 @@ def pixelDensity(x, y):
     # x = np.linspace(min(vec), max(vec), 100)
     # plt.plot(x, mlab.normpdf(x, mean, sigma))
 
-def dist(x, y):
-    return abs(x-y)
-
-def probability(vec, centers):
-    probability = []
-    c1, c2 = centers[0][0], centers[1][0]
-    for val in vec:
-        p1 = 1- dist(val, c1)/(dist(val, c2) + dist(val, c1))
-        p2 = 1- dist(val, c2)/(dist(val, c2) + dist(val, c1))
-
-        probability.append(p1 if p1>p2 else p2)
-    return probability
+# def dist(x, y):
+#     return abs(x-y)
+#
+# def probability(vec, centers):
+#     probability = []
+#     c1, c2 = centers[0][0], centers[1][0]
+#     for val in vec:
+#         p1 = 1- dist(val, c1)/(dist(val, c2) + dist(val, c1))
+#         p2 = 1- dist(val, c2)/(dist(val, c2) + dist(val, c1))
+#
+#         probability.append(p1 if p1>p2 else p2)
+#     return probability
 
 
 def cluster(x):
     x = x.reshape(configs.nFrames, 1)
     kmeans = KMeans(n_clusters=2, random_state=0).fit(x)
 
-    nC1 = kmeans.label_.shape[0] - np.sum(kmeans.label_.shape[0])
-    nC0 = kmeans.label_.shape[0] - nC1
+    nC0 = kmeans.labels_.shape[0] - np.sum(kmeans.labels_)
+    nC1 = kmeans.labels_.shape[0] - nC0
+    print "C(Cluster 0):", nC0
+    print "C(Cluster 1):", nC1
 
-    if nC1 > nC2:
+    if nC1 > nC0:
         print "Centroid 1: Background\nCentroid 0: Foreground"
-        return np.flip(kmeans.cluster_centers_)
+        return kmeans.cluster_centers_[::-1], kmeans.labels_
     else:
         print "Centroid 0: Background\nCentroid 1: Foreground"
-        return kmeans.cluster_centers_
+        return kmeans.cluster_centers_, kmeans.labels_
 
-def getBackgroundPixel(vec, bCent, fCent):
+def getBgPixel(vec, labels, bCent, fCent):
     return bCent
 
-if __name__ == '__main__':
-    X, Y = 200, 300
-    vec = frames[:, X, Y]
-    # pixelDensity(200, 200)
+def getBg():
+    bg = []
+    for i in xrange(configs.dim['x']):
+        for j in xrange(configs.dim['y']):
+            print "Pixel: %i, %i"%(i,j)
+            vec = frames[:, i, j]
 
-    cent = cluster(vec)
-    probs = probability(vec, cent)
+            cent, labels = cluster(vec)
+            # probs = probability(vec, cent)
+            bg.append(getBgPixel(vec, labels, cent[0][0], cent[1][0]))
+
+    bg = np.array(bg)
+    bg = bg.reshape(configs.dim['x'], configs.dim['y'])
+
+    with open('bin/bg_kmeans_hand.pkl', 'wb') as f:
+        pickle.dump(bg, f)
+    plt.imshow(bg, cmap='gray')
+
+if __name__ == '__main__':
+    getBg()
+    # X, Y = 225, 300
+    #
+    # vec = frames[:,X,Y]
+    # pixelDensity(X, Y)
+    # print cluster(vec)
 
     plt.show()
